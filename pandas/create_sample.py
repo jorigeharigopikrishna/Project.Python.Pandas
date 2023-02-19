95250ef12750a308f67fdd0b63c46238c39f8f7b
Hari Gopi Krishna Jorige <63456015+jorigeharigopikrishna@users.noreply.github.com>
2023-02-19 21:17:55 +0530

    edit sample from 50 to 15.

--- pandas/create_sample.py
+++ pandas/create_sample.py
@@ -2,4 +2,5 @@ import pandas as pandas_package
 
 df_object = pandas_package.read_csv("../Datasets/employees_full_data.csv")
 
-sample_50_object = df_object.sample(50)     # Create sample of 50 data points
+sample_15_object = df_object.sample(15)     # Create sample of 15 data points
+print(sample_15_object)
